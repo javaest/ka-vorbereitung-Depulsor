df6d516b591c73eaf39faf513aa0979be00fba10
Christian Kobler <christian.kobler@acd-elektronik.de>
2024-12-02 15:22:32 +0100

    fix restaurant

--- src/main/java/vorlage/Restaurant.java
+++ src/main/java/vorlage/Restaurant.java
@@ -1,17 +1,30 @@
 package vorlage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant {
     private List<Gericht> speisekarte;
     private List<Bestellung> bestellungen;
 
+    public List<Bestellung> getBestellungen() {
+    	return bestellungen;
+    }
+    
     // Methoden zur Verwaltung von Gerichten und Bestellungen
     public void bestellungAufnehmen(Bestellung bestellung) {
-        // Implementierung
+        bestellungen.add(bestellung);
+    }
+    
+    public void bestellungEntfernen(Bestellung bestellung) {
+    	bestellungen.remove(bestellung);
     }
 
     public void gerichtHinzufuegen(Gericht gericht) {
-        // Implementierung
+        speisekarte.add(gericht);
+    }
+    
+    public Restaurant() {
+    	this.bestellungen = new ArrayList<Bestellung>();
     }
 }
